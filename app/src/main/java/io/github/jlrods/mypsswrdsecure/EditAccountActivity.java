74a6493cca6d06459f597c571ba58285f889dd3a
Jose Leonardo Rodriguez <j_rodriguez@modularauto.ie>
2020-11-05 14:10:34 +0000

    Fix app crash issue when account name contains apostrophe

--- app/src/main/java/io/github/jlrods/mypsswrdsecure/EditAccountActivity.java
+++ app/src/main/java/io/github/jlrods/mypsswrdsecure/EditAccountActivity.java
@@ -94,11 +94,15 @@ public class EditAccountActivity extends DisplayAccountActivity {
         switch (item.getItemId()) {
             case R.id.select_logo_save:
                 Log.d("onOptionsItemSelected","The save button was selected by user in EditAccountActivity class.");
-                final String accountName = this.etAccountName.getText().toString();
+                String accountName = this.etAccountName.getText().toString();
                 long psswrdRenewDate = this.getPsswrdRenewDate();
                 //Check all data input is valid and correct (Account name not in use and valid password renew date, as most data come from dropdown menus which are already valid)
                 //Check the name entered is not empty
                 if(!accountName.equals("")){
+                    //Check account name is available but first check if it contains apostrophe so the sql query contains scape character
+                    if(accountName.contains("'")){
+                        accountName = accountsDB.includeApostropheEscapeChar(accountName);
+                    }//End of if to check the name contains apostrophe
                     //Check account name is available
                     if(!isAccNameUsed(accountName,this.account.get_id())){
                         //If Account name not in use
@@ -207,32 +211,6 @@ public class EditAccountActivity extends DisplayAccountActivity {
                             //Handle the onclick event for the dialog alert buttons, Ok and cancel
                             public void onClick(DialogInterface dialog, int which) {
                                 //If Ok was pressed, call DB method that runs query that deletes an account from Accounts table by passing in it's _id
-                                //But, before deleting the account, check the components that make up the account, check if they are not being used
-                                //any longer and delete them from the DB if required. User names, passwords, questions, categories and resource icons  are the only
-                                //exceptions, where they will be able to exist in DB even when not being used.
-                                //Any other sub-item, not being used in any account will be removed form DB: This means QuestionList,
-                                //QuestionAssignment, Icon if the icon comes from URI
-                                //Check the QuestionList (if applicable) has to be deleted
-//                                if(account.getQuestionList() != null){
-//                                    if(accountsDB.getTimesUsedQuestionList(account.getQuestionList().get_id()) <= 1){
-//                                        //Delete the question assignments
-//                                        accountsDB.deleteRowFromTable(MainActivity.getQuestionassignmentTable(),MainActivity.getQuestionListIdColumn(),account.getQuestionList().get_id());
-//                                        //Delete the list
-//                                        accountsDB.deleteItem(account.getQuestionList());
-//                                    }//End of if statement to check how many times the list is being used
-//                                }//End of if statement to check th question list isn't null
-//                                //Check the icon, if icon comes from URI, delete from DB if used only once
-//                                Icon icon = account.getIcon();
-//                                if(icon != null){
-//                                    if(!icon.getLocation().equals(MainActivity.getRESOURCES()) && !icon.equals(MainActivity.getMyPsswrdSecureLogo())){
-//                                        //Get the number of times used
-//                                        if(accountsDB.getAccountsWithSpecifcValue(MainActivity.getIconIdColumn(),icon.get_id()).getCount() <=1){
-//                                            //Delete icon from DB
-//                                            accountsDB.deleteRowFromTable(MainActivity.getIconTable(),MainActivity.getIdColumn(),icon.get_id());
-//                                        }//End of if statement to check the number of times the icon is being used
-//                                    }//End of if statement to check the isn't a resource file
-//                                }//End of if statement to check the icon isn't null
-
                                 //Now the account can be deleted
                                 if(deleteAccount(accountsDB,account)){
                                     //Call method to update data set displayed on the recycler view and display proper message after adding the grocery to the DB
