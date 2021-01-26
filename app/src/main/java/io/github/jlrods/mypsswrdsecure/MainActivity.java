422f994c0e35ce0bbd2da9fb0d6cfe92218e0c30
Jose Leonardo Rodriguez <j_rodriguez@modularauto.ie>
2021-01-26 08:41:34 +0000

    Include command to update toolbar title on onCreate method to display current category name at top bar

--- app/src/main/java/io/github/jlrods/mypsswrdsecure/MainActivity.java
+++ app/src/main/java/io/github/jlrods/mypsswrdsecure/MainActivity.java
@@ -170,7 +170,6 @@ public class  MainActivity extends AppCompatActivity {
         //Get the tool bar off layout
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //toolbar.setTitle("jlrods");
         final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -371,15 +370,15 @@ public class  MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
         this.setUpLowerCategoryMenu(navigationView.getMenu());
         this.updateNavMenu(navigationView.getMenu(),INDEX_TO_GET_LAST_TASK_LIST_ITEM);
-        //toolbar.setTitle("jlrods2");
         //Once the nave menu has been fully set up, select the item that represents the current category and deselect the Home item, which is selected by default
-        if(currentCategory.get_id() != -1){
+        if(this.currentCategory.get_id() != -1){
             navigationView.getMenu().getItem(0).setCheckable(false);
             navigationView.getMenu().getItem(0).setChecked(false);
             navigationView.getMenu().getItem(getCategoryPositionByID(currentCategory.get_id())).setCheckable(true);
             navigationView.getMenu().getItem(getCategoryPositionByID(currentCategory.get_id())).setChecked(true);
         }//End of if statement to check the category to be selected
-
+        //Set toolbar title based on current category
+        toolbar.setTitle(this.currentCategory.getName());
         //@Fixme: define method in accountsDB class
         Cursor appLoginCursor = accountsDB.getAppLoginCursor(accountsDB.getMaxItemIdInTable(APPLOGGIN_TABLE));
 
